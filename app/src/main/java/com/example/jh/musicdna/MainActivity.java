a2acf5b42b486140e729e509fe91fb507936074e
jinhuizxc <1004260403@qq.com>
2018-02-13 02:57:26 +0800

    Initial commit

---
+++ app/src/main/java/com/example/jh/musicdna/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.jh.musicdna;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 做一款汉化版的musicdna项目 -2018/2/13
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
